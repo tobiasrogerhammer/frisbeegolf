6e62bf1cf85a9237e98b78acb7db8436e9adc05a
thammer <tobias@hammerhome.no>
2023-06-14 23:46:34 +0200

    sick updates

--- friebee-stats/src/App.js
+++ friebee-stats/src/App.js
@@ -1,9 +1,15 @@
-import React, { useState } from 'react';
-import 'chart.js';
-import 'chartjs-adapter-date-fns';
-import { Line } from 'react-chartjs-2';
-import { LinearScale, CategoryScale, PointElement, LineElement, Chart } from 'chart.js';
-import styles from './frisbee.module.css';
+import React, { useState } from "react";
+import "chart.js";
+import "chartjs-adapter-date-fns";
+import { Line } from "react-chartjs-2";
+import {
+  LinearScale,
+  CategoryScale,
+  PointElement,
+  LineElement,
+  Chart,
+} from "chart.js";
+import styles from "./frisbee.module.css";
 
 Chart.register(LinearScale, CategoryScale, PointElement, LineElement);
 
@@ -19,35 +25,41 @@ const App = () => {
   const [games, setGames] = useState([]); // State to store the list of games
   const [currentGame, setCurrentGame] = useState(null); // State to track the current game
   const [currentHole, setCurrentHole] = useState(0); // State to track the current hole
-  const [currentLocation, setCurrentLocation] = useState('Ekeberg'); // State to track the current location
+  const [currentLocation, setCurrentLocation] = useState("Ekeberg"); // State to track the current location
 
   const addGame = (location) => {
     const newGame = {
       id: games.length + 1,
       location: location,
       parData: [], // Update the parData based on the selected location
-      userScores: getInitialScores(18, []), // Update the userScores based on the selected location
+      userScores: [], // Initialize userScores as an empty array
     };
-  
+
     switch (location) {
-      case 'Langhus':
-        newGame.parData = [4, 3, 3, 4, 3, 3, 3, 4, 4, 3, 4, 4, 3, 3, 4, 4, 3, 4]; // Example par data for Langhus
+      case "Langhus":
+        newGame.parData = [
+          3, 3, 4, 4, 3, 4, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3,
+        ]; // Example par data for Langhus
         break;
-      case 'Ekeberg':
-        newGame.parData = [3, 3, 4, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3]; // Example par data for Ekeberg
+      case "Ekeberg":
+        newGame.parData = [
+          3, 3, 4, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3,
+        ]; // Example par data for Ekeberg
         break;
-      case 'Myrvoll':
-        newGame.parData = [4, 3, 4, 3, 4, 3, 3, 3, 4, 3, 4, 3, 3, 4, 4, 3, 4, 4]; // Example par data for Myrvoll
+      case "Myrvoll":
+        newGame.parData = [3, 3, 3, 3, 3, 3, 3, 3, 3]; // Example par data for Myrvoll
         break;
       default:
         break;
     }
 
-    newGame.userScores = getInitialScores(18, newGame.parData); // Set the userScores to the corresponding par values
-  
+    newGame.userScores = getInitialScores(
+      newGame.parData.length,
+      newGame.parData
+    ); // Set the userScores to the corresponding par values
+
     setGames([...games, newGame]);
   };
-  
 
   const handleScoreChange = (gameId, index, score) => {
     if (score < 1) {
@@ -91,25 +103,37 @@ const App = () => {
       <h1 className={styles.title}>Ekeberg Frisbee Golf</h1>
       {games.map((game) => (
         <div key={game.id} className={styles.gameContainer}>
-          <h2>Game {game.id} At {game.location}</h2>
-          <button className={styles.selectGameBtn} onClick={() => selectGame(game.id)}>Select Game</button>
-          <div className={styles.totalPar}>Total Par: {game.parData.reduce((acc, score) => acc + score, 0)}</div>
-          <div className={styles.totalScore}>Total Score: {game.userScores.reduce((acc, score) => acc + score, 0)}</div>
+          <h2>
+            Game {game.id} At {game.location}
+          </h2>
+          <button
+            className={styles.selectGameBtn}
+            onClick={() => selectGame(game.id)}
+          >
+            Select Game
+          </button>
+          <div className={styles.totalPar}>
+            Total Par: {game.parData.reduce((acc, score) => acc + score, 0)}
+          </div>
+          <div className={styles.totalScore}>
+            Total Score:{" "}
+            {game.userScores.reduce((acc, score) => acc + score, 0)}
+          </div>
           <Line
             data={{
               labels: game.parData.map((_, index) => `Hole ${index + 1}`),
               datasets: [
                 {
-                  label: 'Par',
+                  label: "Par",
                   data: game.parData,
-                  borderColor: 'red',
+                  borderColor: "red",
                   borderWidth: 1,
                   fill: false,
                 },
                 {
-                  label: 'User',
+                  label: "User",
                   data: game.userScores,
-                  borderColor: 'blue',
+                  borderColor: "blue",
                   borderWidth: 1,
                   fill: false,
                 },
@@ -118,11 +142,11 @@ const App = () => {
             options={{
               scales: {
                 y: {
-                  type: 'linear',
+                  type: "linear",
                   beginAtZero: true,
                   title: {
                     display: true,
-                    text: 'Scores',
+                    text: "Scores",
                   },
                   ticks: {
                     stepSize: 1,
@@ -131,42 +155,70 @@ const App = () => {
                 x: {
                   title: {
                     display: true,
-                    text: 'Holes',
+                    text: "Holes",
                   },
                 },
               },
             }}
           />
           <div className={styles.holeInfo}>
-            <button className={styles.prevHoleBtn} onClick={handlePrevHole} disabled={currentHole === 0}>
+            <button
+              className={styles.prevHoleBtn}
+              onClick={handlePrevHole}
+              disabled={currentHole === 0}
+            >
               Previous Hole
             </button>
             {currentGame && (
               <div>
-                <h3 className={styles.updateScoreTitle}>Score for hole: {currentHole + 1}</h3>
+                <h3 className={styles.updateScoreTitle}>
+                  Score for hole: {currentHole + 1}
+                </h3>
                 <input
                   type="number"
                   value={games[currentGame - 1].userScores[currentHole]}
-                  onChange={(e) => handleScoreChange(currentGame, currentHole, parseInt(e.target.value))}
+                  onChange={(e) =>
+                    handleScoreChange(
+                      currentGame,
+                      currentHole,
+                      parseInt(e.target.value)
+                    )
+                  }
                   className={styles.scoreInput}
                 />
               </div>
             )}
-            <button className={styles.nextHoleBtn} onClick={handleNextHole} disabled={currentHole === 17}>
+            <button
+              className={styles.nextHoleBtn}
+              onClick={handleNextHole}
+              disabled={currentHole === 17}
+            >
               Next Hole
             </button>
           </div>
         </div>
       ))}
-          <div>
-          <label htmlFor="locationSelect" className={styles.label}>New game at </label>
-          <select id="locationSelect" value={currentLocation} onChange={(e) => setCurrentLocation(e.target.value)} className={styles.select}>
-            <option value="Langhus">Langhus</option>
-            <option value="Ekeberg">Ekeberg</option>
-            <option value="Myrvoll">Myrvoll</option>
-          </select>
+      <div>
+        <label htmlFor="locationSelect" className={styles.label}>
+          New game at{" "}
+        </label>
+        <select
+          id="locationSelect"
+          value={currentLocation}
+          onChange={(e) => setCurrentLocation(e.target.value)}
+          className={styles.select}
+        >
+          <option value="Langhus">Langhus</option>
+          <option value="Ekeberg">Ekeberg</option>
+          <option value="Myrvoll">Myrvoll</option>
+        </select>
       </div>
-       <button className={styles.addGameBtn} onClick={() => addGame(currentLocation)}>Create Game</button>
+      <button
+        className={styles.addGameBtn}
+        onClick={() => addGame(currentLocation)}
+      >
+        Create Game
+      </button>
     </div>
   );
 };
